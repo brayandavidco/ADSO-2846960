7a45c26d4b31b9e965be8a245ea5e04987632cd5
09marlen <marlensalcedo12@gmail.com>
2025-02-27 06:27:57 -0500

    hola que tal

--- ADSO2846960/src/adso2846960/ADSO2846960.java
+++ ADSO2846960/src/adso2846960/ADSO2846960.java
@@ -16,7 +16,7 @@ public class ADSO2846960 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //hola que tal
+        //hola que tal...
     }
     
 }
